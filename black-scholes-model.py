3b4ad5f611645594b26546f6c2355ee8e9a0e75b
wiederin <wiederin@usc.edu>
2022-06-12 22:41:54 +0200

    initial implementation of black scholes model

--- black-scholes-model.py
+++ black-scholes-model.py
@@ -1,6 +1,32 @@
+from math import log, sqrt, pi, exp
+from scipy.stats import norm
 
-
-def black_scholes_model ():
-    """
+"""
         Price an option using the Black-Scholes model
-    """
+
+        C = call option price
+
+        S = current stock price
+
+        K = strike price
+
+        r = risk-free interest rate
+
+        t = time to maturity
+
+        N = a normal distribution
+"""
+
+def d1(s, k, t, r, sigma):
+    return(log(s/k) + (r + sigma**2/2.) * t) / (sigma * sqrt(t))
+
+
+def d2(s, k, t, r, sigma):
+    return d1(s, k, t, r, sigma) - sigma * sqrt(t)
+
+
+def black_scholes_call(s, k, t, r, sigma):
+    return s * norm.cdf(d1(s, k, t, r, sigma)) - k * exp(-r * t) * norm.cdf(d2(s, k, t, r, sigma))
+
+def black_scholes_put(s, k, t, r, sigma):
+    return k * exp(-r * t) - s * black_scholes_call(s, k, t, r, sigma)
